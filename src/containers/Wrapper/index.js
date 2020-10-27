d3434bcb5aad6a51a7312e151f93b36f0cacacb7
Yeim <brielalica@gmail.com>
2020-10-27 18:41:09 -0400

    Third task

--- src/containers/Wrapper/index.js
+++ src/containers/Wrapper/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-const Wrapper = ({children}) => {
+const Wrapper = ({ children }) => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
-            {children}
-        </div>
+
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px'  }}>
+                <div className="site-layout-content" style={{ display:'flex', justifyContent:'center', alignItems:'center',  width:'100%', height: '77vh' }}>{children}</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}> ©2020 Buil with <span>♥</span> by &lt;y &gt;eim&lt;/y&gt;</Footer>
+        </Layout>
     )
 }
 
